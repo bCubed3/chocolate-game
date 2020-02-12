02087e2846c4cd0414b364c4f013ed97306e45be
bCubed3 <sf.bcubed@gmail.com>
2020-02-11 17:37:49 -0800

    refactor weight creation

--- enn.py
+++ enn.py
@@ -7,31 +7,23 @@ class EvoNeuralNetwork:
     def __init__(self, sizes, child_weights=0):
         self.sizes = sizes
         self.weights = []
+        self.biases = []
         if child_weights == 0:
-            for layer in range(len(sizes) - 1):
-                self.weights.append([])
-                for node in range(sizes[layer + 1]):
-                    self.weights[layer].append([])
-                    for weight in range(sizes[layer]):
-                        self.weights[layer][node].append(2 * r.random() - 1)
+            for i in range(len(sizes) - 1):
+                self.weights.append(np.random.rand(sizes[i], sizes[i + 1]))
+                self.weights[i] = self.weights[i] - 0.5
+                self.weights[i] = self.weights[i] * 2
+                self.biases.append(np.random.rand(sizes[i], sizes[i + 1]))
+
         else:
             pass
 
     def forward_propagate(self, inp_arr):
         if len(inp_arr) == self.sizes[0]:
-            t_layers = [deepcopy(inp_arr)]
-            for w_layer in self.weights:
-                for n_index, node in enumerate(w_layer):
-                    t_layers.append([])
-                    t_node_value = 0
-                    print(t_layers)
-                    for w_index, weight in enumerate(node):
-                        print(w_index)
-                        t_node_value = t_node_value + t_layers[-2][w_index] * weight
-                    t_layers[-1].append(t_node_value)
+            t_layer = deepcopy(inp_arr)
 
 
 
-nn = EvoNeuralNetwork([3, 3, 3])
-print(nn.forward_propagate([10, 3, 4]))
+nn = EvoNeuralNetwork([3, 2, 4])
 print(nn.weights)
+
